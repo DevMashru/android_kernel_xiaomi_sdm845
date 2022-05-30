e4ea7e6de1667362ce82aa1d47edddec30daa3e8
Dev <devvmashru@protonmail.ch>
2022-05-30 21:05:42 +0530

    Revert "drivers/char: Remove ununsed variable"
    This reverts commit f7cc4978f38006af97ef5e39e92e471d8adc5ef8.

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3971,7 +3971,7 @@ static long fastrpc_device_ioctl(struct file *file, unsigned int ioctl_num,
 	} i;
 	void *param = (char *)ioctl_param;
 	struct fastrpc_file *fl = (struct fastrpc_file *)file->private_data;
-	int size = 0, err = 0;
+	int size = 0, err = 0, req_complete = 0;
 	uint32_t info;
 	static bool isQueryDone;
 
